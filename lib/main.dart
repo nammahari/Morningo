191864e4db2c4da0d7cc7740a8e7177319afffab
Mugilan <86902446+MugilanS@users.noreply.github.com>
2022-11-26 00:47:14 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
-import 'package:morningo/popup.dart';
-// import 'package:morningo/responsive.dart';
-// import 'package:morningo/mobile_body.dart';
-// import 'package:morningo/tab_body.dart';
-// import 'package:morningo/desktop_body.dart';
+import 'package:morningo/responsive.dart';
+import 'package:morningo/mobile_body.dart';
+import 'package:morningo/tab_body.dart';
+import 'package:morningo/desktop_body.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body:Popup(),
+      body:HeroPage(),
     ),
   ));
 }
-// class HeroPage extends StatefulWidget {
-//   const HeroPage({key}) : super(key: key);
-//
-//   @override
-//   State<HeroPage> createState() => _HeroPageState();
-// }
-//
-// class _HeroPageState extends State<HeroPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     final currentWidth = MediaQuery.of(context).size.width;
-//     return const Scaffold(
-//       body: Responsive(mobileBody: Mobile(), tabBody: MyTab(), desktopBody: Desktop()),
-//     );
-//   }
-// }
+class HeroPage extends StatefulWidget {
+  const HeroPage({key}) : super(key: key);
+
+  @override
+  State<HeroPage> createState() => _HeroPageState();
+}
+
+class _HeroPageState extends State<HeroPage> {
+  @override
+  Widget build(BuildContext context) {
+    final currentWidth = MediaQuery.of(context).size.width;
+    return const Scaffold(
+      body: Responsive(mobileBody: Mobile(), tabBody: MyTab(), desktopBody: Desktop()),
+    );
+  }
+}
